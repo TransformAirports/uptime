55b3dfe7b46275e077b54d7cc9aa11a8f55fd9ca
Christian Kessler IV <cgkio@users.noreply.github.com>
2025-08-04 13:51:40 -0400

    Add utility functions for indicator colors

--- website/utils.js
+++ website/utils.js
@@ -3,6 +3,18 @@ function normalizeDeviceType(type) {
   return type.toLowerCase().replace(/s$/, '');
 }
 
+// Map a power status boolean to the corresponding indicator color.
+// `true` indicates the device has power and returns green, otherwise red.
+function getPowerColor(power) {
+  return power ? 'green' : 'red';
+}
+
+// Map an alarm status boolean to the corresponding indicator color.
+// `true` indicates an alarm condition and returns red, otherwise green.
+function getAlarmColor(alarm) {
+  return alarm ? 'red' : 'green';
+}
+
 if (typeof module !== 'undefined') {
-  module.exports = { normalizeDeviceType };
+  module.exports = { normalizeDeviceType, getPowerColor, getAlarmColor };
 }
